cc50f51805c4ea2d59eee10b13201c0311c8678d
Sally Fisher <sqfisher@gmail.com>
2015-10-13 15:23:46 -0500

    Assignment completed: Accepting user name as input in console and prints encouraging message spelling the letters of their name

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the user's name and print it.
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, " + name);
+
+            //Cheer the user on.
+            foreach (char letter in name)
+            {
+                string vowels = "aeiouhlmnrsx";
+                string article = "a";
+                if (vowels.Contains(Char.ToLower(letter)) == true)
+                {
+                    article = "an";
+                }
+                Console.WriteLine("Give me " + article + " " + letter);
+            }
+            Console.WriteLine(name + " is GRAND!");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
