2df752b4ba037c29d20c8b7ef04dea60fd49a8b0
Sally Fisher <sqfisher@gmail.com>
2015-10-15 02:27:47 -0500

    Accepts birthday user input and outputs number of days until birthday, or "happy birthday", when appropriate.

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -14,7 +14,9 @@ namespace Cheers
             Console.WriteLine("What's your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Hi, " + name);
-
+            Console.WriteLine("When is your birthday? Please use MM/DD format.");
+            string input = Console.ReadLine();
+            DateTime birthday = DateTime.ParseExact(input, "MM/dd", null);
             //Cheer the user on.
             foreach (char letter in name)
             {
@@ -27,6 +29,19 @@ namespace Cheers
                 Console.WriteLine("Give me " + article + " " + letter);
             }
             Console.WriteLine(name + " is GRAND!");
+            TimeSpan diff = birthday.Subtract(DateTime.Today);
+            if (diff.Days > 0)
+            {
+                Console.WriteLine("Your birthday is {0} days away!", diff.Days);
+            }
+            else if (diff.Days < 0)
+            {
+                Console.WriteLine("Your birthday is {0} days away!", diff.Days + 365 + 1);
+            }
+            else
+            {
+                Console.WriteLine("Happy Birthday, {0}!!!", name);
+            }
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
